a6b5f2d329277afc7fbfd8d9b4732a47f934b2a8
nippunb18 <nippun.b18@gmail.com>
2026-08-27 23:00:37 +0530

    This is v3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        System.out.printf("This is version v2");
+        System.out.printf("This is version v3");
 
 
         for (int i = 1; i <= 5; i++) {
