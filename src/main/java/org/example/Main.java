76ae631c01cb329922a598cfcfc5f16747524f75
nippunb18 <nippun.b18@gmail.com>
2026-08-28 08:56:59 +0530

    new commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        System.out.printf("This is version design v6.1.2026");
-        System.out.printf("This is version v3");
+        System.out.printf("This is version design v7.2026");
+        System.out.printf("This is version vsdfsdf3");
 
 
         for (int i = 1; i <= 5; i++) {
